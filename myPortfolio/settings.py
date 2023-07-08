e71768e9c29ac235ead4fb6e3566de296d3d2a9b
rocktim <rocktimsharma82@gmail.com>
2023-07-08 21:05:26 +0530

    debugging 2

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS=[
 
 
 # Following settings only make sense on production and may break development environments.
-if not DEBUG:
+if DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
