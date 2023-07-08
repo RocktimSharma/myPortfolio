4b931ebc08443d477f4846c4e35569e254a84bf6
rocktim <rocktimsharma82@gmail.com>
2023-07-08 21:03:49 +0530

    debugging

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env()
 SECRET_KEY = 'django-insecure-u5or57qk$n!6-5c6!+4fr7k3ell#vk5z3ze!son$buh=eub#px'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://rocktimsharma.onrender.com']
 
